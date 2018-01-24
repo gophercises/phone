c7eda2581b00c487793f8132943e92ec9eb244ae
Jon Calhoun <joncalhoun@gmail.com>
2018-01-24 00:49:25 -0500

    Wrote the normalization code

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"regexp"
+)
+
+func normalize(phone string) string {
+	re := regexp.MustCompile("\\D")
+	return re.ReplaceAllString(phone, "")
+}
+
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '0' && ch <= '9' {
+// 			buf.WriteRune(ch)
+// 		}
+// 	}
+// 	return buf.String()
+// }
